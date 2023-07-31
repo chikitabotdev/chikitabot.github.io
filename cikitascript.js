01aa3907c6946b0e29df3bc57f26211177f2138c
Mustafa Baypara <94145699+MustafaBaypara@users.noreply.github.com>
2023-07-31 18:18:05 +0300

    Update cikitascript.js

--- cikitascript.js
+++ cikitascript.js
@@ -3,10 +3,10 @@ document.addEventListener('DOMContentLoaded', function() {
     const maxheight = body.scrollHeight - window.innerHeight
 
     window.addEventListener('scroll', () => {
-        if (window.scrollY > (maxheight / 2) ) {
+        if (window.scrollY > (maxheight * 0.5) ) {
             body.style.backgroundColor = "#ff0000";
         }
-        else if (window.scrollY > ((maxheight / 2) + (maxheight / 4))) {
+        else if (window.scrollY > (maxheight * 0.75)) {
             body.style.backgroundColor = "#ffe700";
         }
         else {
