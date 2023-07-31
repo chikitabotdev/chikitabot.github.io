53282edd9e6765368b348e8a6192151f2a61bcba
Mustafa Baypara <94145699+MustafaBaypara@users.noreply.github.com>
2023-07-31 16:51:29 +0300

    Update cikitascript.js

--- cikitascript.js
+++ cikitascript.js
@@ -2,9 +2,9 @@ const element = document.querySelector('.body');
 
 window.addEventListener('scroll', () => {
   // Sayfa kaydırıldıkça scrollY değeri artar, bu değeri arka plan rengiyle ilişkilendiriyoruz
-  const scrollY = window.scrollY;
-  const maxScroll = document.body.scrollHeight - window.innerHeight;
-
-  const background = "background: radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(255, 182, 212, 0.8) 100%);"
-  element.style.background = background;
+  const scrollhalfpage = window.innerHeight / 2;
+  
+  if (window.scrollY > scrollhalfpage) {
+    const background = "background: radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(255, 182, 212, 0.8) 100%);"
+    element.style.background = background;}
 });
