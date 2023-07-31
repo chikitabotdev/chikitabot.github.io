f4aed695b7f8d7a9fbecaab08557c3b864649b96
Mustafa Baypara <94145699+MustafaBaypara@users.noreply.github.com>
2023-07-31 16:49:14 +0300

    Update cikitascript.js

--- cikitascript.js
+++ cikitascript.js
@@ -5,11 +5,6 @@ window.addEventListener('scroll', () => {
   const scrollY = window.scrollY;
   const maxScroll = document.body.scrollHeight - window.innerHeight;
 
-  // Arka plan rengini, kaydırma yüzdesine göre değiştiriyoruz
-  const hue = (scrollY / maxScroll) * 360; // 0 ile 360 arasında bir renk tonu alacak
-  const saturation = 100; // %100 doygunluk, yani canlı bir renk
-  const lightness = 50; // %50 parlaklık, orta tonlarda bir renk
-
   const background = "background: radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(255, 182, 212, 0.8) 100%);"
   element.style.background = background;
 });
