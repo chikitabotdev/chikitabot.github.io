88d1a4aec5cf7d3b5c07a62a0c45be36ef151809
Mustafa Baypara <94145699+MustafaBaypara@users.noreply.github.com>
2023-07-31 17:33:05 +0300

    color

--- cikitascript.js
+++ cikitascript.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
     const body = document.body;
-    const scrollhalfpage = window.innerHeight / 2;
 
     window.addEventListener('scroll', () => {
-        if (window.scrollY > scrollhalfpage) {
+        if (window.scrollY > window.innerHeight / 2) {
             body.style.backgroundColor = "#ff0000";
         }
-        else {
-            body.style.backgroundColor = "";
+        if (window.scrollY > window.innerHeight / 3) {
+            body.style.backgroundColor = "#ff0000";
         }
+            body.style.backgroundColor = "#ffe700";
     });
 });
