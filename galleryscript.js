bc3f3c6ddb2e39cae05b6bad6c7ec82f06c02f03
Mustafa Baypara <94145699+MustafaBaypara@users.noreply.github.com>
2023-10-26 07:07:51 +0300

    js

--- galleryscript.js
+++ galleryscript.js
@@ -1,3 +1,54 @@
+let req = new XMLHttpRequest();
+
+function formatNumber(number) {
+    if (number >= 1000000) {
+      return (number / 1000000).toFixed(2) + 'M'; // Milyon
+    } else if (number >= 1000) {
+      return (number / 1000).toFixed(1) + 'K'; // Bin
+    } else {
+      return number.toString(); // Bin ve milyon altındaki sayılar
+    }
+  }
+
+document.addEventListener('DOMContentLoaded', function() {
+    req.onreadystatechange = () => {
+        if (req.readyState == XMLHttpRequest.DONE) {
+            const responseJSON = JSON.parse(req.responseText);
+            var uyesayisi = responseJSON.member_count;
+            document.getElementsByClassName("member-number")[0].innerHTML = formatNumber(uyesayisi);
+            var serversayisi = responseJSON.server_count;
+            document.getElementsByClassName("server-number")[0].innerHTML = formatNumber(serversayisi);
+        }
+      };
+    
+
+  });
+
+req.open("GET", "https://api.cikita.chikitabot.net:58731/api/server_count", true);
+req.send();
+
+document.addEventListener('DOMContentLoaded', function() {
+    window.addEventListener('scroll', () => {
+        const body = document.body;
+        const maxheight = (body.scrollHeight - window.innerHeight) / 4;
+        if (window.scrollY > (maxheight) ) {
+          document.getElementsByClassName("cikiheader")[0].style.boxShadow = "rgb(1 1 1 / 10%) 0 4px 15px -1px, rgb(0 0 0 / 6%) 0 2px 10px -1px";
+        }
+        else {
+          document.getElementsByClassName("cikiheader")[0].style.boxShadow = "none";
+        }
+    });
+});
+function changeColorBG(id, color) {
+    document.getElementById(id).style.backgroundColor = color;
+}
+function changeColor(id, color) {
+    document.getElementById(id).style.color = color;
+}   
+function refreshPage() {
+    window.location.reload();
+}
+
 
 document.addEventListener('DOMContentLoaded', function() {
 // JSON verilerini almak için bir GET isteği yapın
@@ -42,7 +93,6 @@ fetch('https://api.cikita.chikitabot.net:58731/api/archivecikita')
   .catch(error => {
     console.error('Veriler alınırken hata oluştu:', error);
   });
-
 
 });
 
